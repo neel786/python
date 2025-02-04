751a0356952e073ad826969d0b83ed6c3f48b8e6
neel786 <86419621+neel786@users.noreply.github.com>
2025-02-04 15:56:20 +0600

    Update firstprogram&BasicDataTypes.py

--- firstprogram.py
+++ firstprogram.py
@@ -1,4 +1,4 @@
-# python
+# python: 4 February 2025
 
 print("Hello World")
 
@@ -7,3 +7,118 @@ b=20
 
 c= a+b
 print(c)
+
+str1 = "Hello"
+str2 = "World"
+print(type(str1))
+
+#concatenate Two String.
+str3 = str1 + str2
+print(str3)
+print(type(st3))
+print(type(str3))
+
+# Check Length of String
+
+
+"""
+Length of string
+"""
+print(len(str3))
+
+
+#Access A particular character of string
+str3[0]
+
+str3[1]
+
+#Print Repeating String
+print("*********************")
+
+print ("*" * 50)
+
+
+#Splitting String
+str1="This is , Good"
+
+str2=str1.split(',')
+
+str2[0] 
+str2[1]
+
+#count
+
+str1.count(' ')
+
+# Replace
+
+str1="Hello World"
+
+str1.replace("Hello", "Hi")
+
+#
+
+str1.find("Hello")
+
+str1="Hi World"
+str1.find("Hello")
+
+#============================
+
+#coplex type
+
+
+a=10
+b=4.0
+
+
+a = 3 + 4j
+b = 5 + 5j
+
+c = a+b
+
+
+print(c.real)
+print(c.imag)
+
+#=============================
+
+#Boolean
+c=True
+
+print(type(c))
+
+status = bool(input('OK to proceed: '))
+
+#Creating a dictionary
+cities = {
+           'wales':'Cardiff',
+           'England':'London',
+           'Scotland': 'Edinburgh',
+          
+        
+        }
+
+#===========================
+
+print(cities['wales'])
+
+
+dict1={}
+
+#Adding a New Entry
+
+cities['France'] = 'Paris'
+
+#===========================
+#changing the value by using keys
+cities['Wales']='Cardiff1'
+
+cities['wales']='Cardiff2'
+
+
+#===========================
+#Remove Entry
+cities.pop('Wales')
+
+print(type(status))
